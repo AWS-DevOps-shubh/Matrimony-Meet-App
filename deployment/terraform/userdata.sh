134ce6ab7ad828f45101ad24b9d94a0a1dbcbf62
AWS-DevOps-shubh <devshubh2204@gmail.com>
2025-03-20 22:56:55 +0530

    Create userdata.sh

---
+++ deployment/terraform/userdata.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install and configure docker on ec2
+sudo apt-get update
+sudo apt install docker.io -y 
+sudo service docker start
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+
+#Install KIND and kubectl
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+VERSION="v1.30.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+rm -f kubectl
+rm -rf kind
+
+echo "kind & kubectl installation complete."
